49bc69a8a113bf23435c24a754427ccd9c58ac2a
MaxDGold <maksim.moskalyuk.94@gmail.com>
2023-03-07 20:49:44 +0300

    1. Поправил класс ThreadState там образом, чтобы в нем создавалось две нити. Каждая нить должна вывести свое имя на консоль.
    2. Нить main должна дождаться завершения этих нитей и вывести на консоль сообщение, что работа завершена.

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,30 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}
+        );
+        Thread second = new Thread(
+                () -> {}
+        );
+
+        System.out.println("first#Thread  - " + first.getState());
+        System.out.println("second#Thread - " + second.getState());
+
+        first.start();
+        second.start();
+
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.println("first#Thread  - " + first.getState());
+            System.out.println("second#Thread - " + second.getState());
+        }
+
+        System.out.println("first#Thread  - " + first.getState());
+        System.out.println("second#Thread - " + second.getState());
+
+        System.out.println("Работа завершена");
+    }
+
+}
