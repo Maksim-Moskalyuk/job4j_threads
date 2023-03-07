f82f71e86bffcb128fec877608f0b453ed1afcdc
MaxDGold <maksim.moskalyuk.94@gmail.com>
2023-03-07 21:16:21 +0300

    1. Создать класс Wget 2. В методе main необходимо симулировать процесс загрузки от 1 до 100

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    for (int index = 0; index <= 100; index++) {
+                        try {
+                            Thread.sleep(1000);
+                            System.out.print("\rLoading : " + index  + "%");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("\nЗагрузка завершена");
+                }
+        );
+        thread.start();
+    }
+
+}
