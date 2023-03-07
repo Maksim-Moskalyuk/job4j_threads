721983286861de61747848400a3b6a6fa1b6363c
MaxDGold <maksim.moskalyuk.94@gmail.com>
2023-03-08 00:31:56 +0300

    1. Метод выводит в консоль значок загрузки

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable{
+
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        int index = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r Loading ... " + process[index]);
+            index = (index + 1) % process.length;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Thread progress = new Thread(new ConsoleProgress());
+            progress.start();
+            Thread.sleep(5000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
+            progress.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
