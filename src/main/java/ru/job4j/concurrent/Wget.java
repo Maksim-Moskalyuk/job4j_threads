0116c02141f842f24c3567c23a37aed77003d894
Maksim-Moskalyuk <maksim.moskalyuk.94@gmail.com>
2023-03-10 00:13:54 +0300

    Wget

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -7,7 +7,7 @@ public class Wget {
                 () -> {
                     for (int index = 0; index <= 100; index++) {
                         try {
-                            Thread.sleep(100);
+                            Thread.sleep(1000);
                             System.out.print("\rLoading : " + index  + "%");
                         } catch (InterruptedException e) {
                             e.printStackTrace();
