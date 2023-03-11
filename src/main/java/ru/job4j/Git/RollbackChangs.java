df88022f3aec8fa0df699286c5833f0f4defb6c7
MaxDGold <maksim.moskalyuk.94@gmail.com>
2023-03-11 23:49:54 +0300

    changes 4

--- src/main/java/ru/job4j/Git/RollbackChangs.java
+++ src/main/java/ru/job4j/Git/RollbackChangs.java
@@ -3,7 +3,6 @@ package ru.job4j.Git;
 public class RollbackChangs {
 
     public static void main(String[] args) {
-        System.out.println("Произошли измениеия 2");
         System.out.println("Произошли измениеия 1");
     }
 }
