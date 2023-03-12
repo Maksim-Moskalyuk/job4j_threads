f16e05bc5c0a3b098dab4eb4a5538226b901fb38
MaxDGold <maksim.moskalyuk.94@gmail.com>
2023-03-12 15:36:02 +0300

    Прерывание нити [#1019]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -16,7 +16,6 @@ public class ConsoleProgress implements Runnable {
             Thread progress = new Thread(new ConsoleProgress());
             progress.start();
             Thread.sleep(5000); /* симулируем выполнение параллельной задачи в течение 5 секунд. */
-            progress.interrupt();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
