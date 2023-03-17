430bddcb50716783fb28beaecc094b50455374f1
Maksim-Moskalyuk <maksim.moskalyuk.94@gmail.com>
2023-03-18 00:00:56 +0300

    4. Режим ожидания. [#231217]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -3,6 +3,7 @@ package ru.job4j.concurrent;
 public class Wget {
 
     public static void main(String[] args) {
+
         Thread thread = new Thread(
                 () -> {
                     for (int index = 0; index <= 100; index++) {
