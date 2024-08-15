4918ce88285f07b35684fc11970e2bd21efb3ac6
Maksim-Moskalyuk <maksim.moskalyuk.94@gmail.com>
2024-08-15 17:54:53 +0300

    3.1. Добавить изменения в последний коммит. [#504829 #399709]
    Добавляем изменение с amend

--- src/main/java/ru/job4j/git/RollbackChangs.java
+++ src/main/java/ru/job4j/git/RollbackChangs.java
@@ -1,9 +1,9 @@
-package ru.job4j.Git;
+package ru.job4j.git;
 
 public class RollbackChangs {
 
     public static void main(String[] args) {
         System.out.println("Произошли измениеия 1");
-        System.out.println("Произошли измениеия 2");
+        System.out.println("Добавляем с использованием amend");
     }
 }
